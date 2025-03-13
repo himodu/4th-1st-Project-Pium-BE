dc27bb739077b02ce8bd71807029f93f49deafa3
yso8296 <yso829612@naver.com>
2025-03-13 14:26:23 +0900

    프로젝트 세팅

---
+++ src/test/java/gdg/pium/PiumApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.pium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
