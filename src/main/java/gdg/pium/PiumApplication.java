8b6650ddf0c07774290e4de28327d36c3b6bcf0f
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2025-05-11 23:07:04 +0900

    feat: 서버 시간대 설정

--- src/main/java/gdg/pium/PiumApplication.java
+++ src/main/java/gdg/pium/PiumApplication.java
@@ -1,10 +1,19 @@
 package gdg.pium;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PiumApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PiumApplication.class, args);
